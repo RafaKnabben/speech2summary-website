83c55b833afcf6464f463b2475c5bacbc1b9eba8
Rafael Knabben <rknabben@gmail.com>
2021-11-25 21:42:07 +0100

    change to app.py

--- app.py
+++ app.py
@@ -18,5 +18,5 @@ params = {"url": url_input}
 if button:
     res = requests.get(url, params=params)
     text = res.json()
-    summ = text['summary']
+    summ = text['Summarized text']
     st.write(summ)
