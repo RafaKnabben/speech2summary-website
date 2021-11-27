005751701ca16fd40d5974cb39cf6e9a6f10fe00
Rafael Knabben <rknabben@gmail.com>
2021-11-27 11:47:06 +0100

    capital letters function

--- app.py
+++ app.py
@@ -15,8 +15,23 @@ button
 
 params = {"url": url_input}
 
+def capital_letters(text):
+    capitalized = ""
+    if text[0] != text[0].capitalize():
+        capitalized = capitalized + text[0].capitalize()
+        text = text[1:]
+
+    for num in range(len(text)):
+        if text[num - 2] + text[num - 1] != ". ":
+            capitalized = capitalized + text[num]
+        if text[num - 2] + text[num - 1] == ". ":
+            capitalized = capitalized + text[num].capitalize()
+
+    return capitalized
+
 if button:
     res = requests.get(url, params=params)
     text = res.json()
     summ = text['Summarized text']
+    summ = capital_letters(summ)
     st.write(str(summ))
