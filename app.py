96b86cfbd742f9fe44fa4162c392fade45e91111
Rafael Knabben <rknabben@gmail.com>
2021-12-01 11:28:18 +0100

    updating app.py

--- app.py
+++ app.py
@@ -1,19 +1,11 @@
 import streamlit as st
-from pydub import AudioSegment
 import requests
 
-st.markdown('''## Video Audio Summarizer
-''')
+st.set_page_config(page_title="Speech Summarizer",
+                   initial_sidebar_state="expanded",
+                   layout="wide")
 
-url = "https://test--2511-m67ja235na-ew.a.run.app/all_steps_test"
-
-form = st.form(key="uploading")
-url_input = form.text_input("Video link", "http://")
-
-button = form.form_submit_button("Submit!")
-button
-
-params = {"url": url_input}
+url = "https://video-audio-summ-ext-abs-m67ja235na-ew.a.run.app/abs_ext_all_test"
 
 CSS_summary = """
     p {
@@ -21,23 +13,34 @@ CSS_summary = """
     }
 """
 
-def capital_letters(text):
-    capitalized = ""
-    if text[0] != text[0].capitalize():
-        capitalized = capitalized + text[0].capitalize()
-        text = text[1:]
+'''
+# Video Summarizer
+'''
 
-    for num in range(len(text)):
-        if text[num - 2] + text[num - 1] != ". ":
-            capitalized = capitalized + text[num]
-        if text[num - 2] + text[num - 1] == ". ":
-            capitalized = capitalized + text[num].capitalize()
+st.markdown(
+'''Our application allows users to create a summary of a given speech on video or audio.
+Simply use the features below and it will output an abstractive summary from your given media input.
+    ''')
 
-    return capitalized
+col1, col2 = st.columns(2)
 
-if button:
-    res = requests.get(url, params=params)
-    text = res.json()
-    summ = text['Summarized text']
-    summ = capital_letters(summ)
-    st.write(str(summ), f'<style>{CSS_summary}</style>', unsafe_allow_html=True)
+with col1:
+
+    form = col1.form(key="uploading")
+    url_input = form.text_input("Video link", "http://")
+
+    button = form.form_submit_button("Submit!")
+    button
+
+    params = {"url": url_input}
+
+    if button:
+        res = requests.get(url, params=params)
+        text = res.json()
+        summ = text['Summarized text']
+        col2.write(str(summ), f'<style>{CSS_summary}</style>', unsafe_allow_html=True)
+
+with col2:
+    """
+    Here's your summaries
+    """
